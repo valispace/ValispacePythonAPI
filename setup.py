498dd4ac635dcd85f87509718a95d4344f9cd496
Louise Lindblad <louise@valispace.com>
2017-12-04 16:26:34 +0000

    new setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
     name='valispace',
-    packages=['valispace'],  # this must be the same as the name above
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     version='0.1',
-    description='Valispace python API',
+    description='Valispace Python API',
     author='Valispace',
     author_email='contact-us@valispace.com',
+    license='MIT',
     url='https://github.com/valispace/ValispacePythonAPI',  # use the URL to the github repo
-    download_url='https://github.com/valispace/ValispacePythonAPI/archive/0.1.tar.gz',  # I'll explain this in a second
+    # download_url='https://github.com/valispace/ValispacePythonAPI/archive/0.1.tar.gz',
     keywords=[],  # arbitrary keywords
     classifiers=[],
 )
