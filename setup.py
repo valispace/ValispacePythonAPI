0d1da07b0d4f53d57df07e6170c42ed375805e29
Tiago Farto <1585571+xernobyl@users.noreply.github.com>
2020-03-05 14:58:33 +0000

    Incremented version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='valispace',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['requests', 'six'],
-    version='0.1.15',
+    version='0.1.16',
     description='Valispace Python API',
     author='Valispace',
     author_email='support@valispace.com',
