a3842b9882ad9dc8a49472b85ca2f00162ab9fbf
Tiago <1585571+xernobyl@users.noreply.github.com>
2021-08-06 15:46:43 +0100

    First commit.

--- valispace/__init__.py
+++ valispace/__init__.py
@@ -2,6 +2,115 @@
 # -*- coding: utf-8 -*-
 
 
+from logging import Logger
+import typing
+import httpx
+
+
+"""
+Valispace public python API, version 2
+"""
+
+
+class ValispaceException(Exception):
+    pass
+
+
+class Valispace:
+    def __init__(
+        self,
+        options: typing.Optional[typing.Dict[str, typing.Union[str, typing.Any]]] = None,
+        username: typing.Optional[str] = None,
+        password: typing.Optional[str] = None,
+        url: typing.Optional[str] = None
+    ):
+        self.total_requests: int = 0
+
+        username = options.get("username", username) if options is not None else None
+        if not username:
+            raise ValispaceException("Username not provided")
+
+        password = options.get("password", password) if options is not None else None
+        if not password:
+            raise ValispaceException("Password not provided")
+
+        url = options.get("url", url) if options is not None else None
+        if not url:
+            raise ValispaceException("URL not provided")
+        else:
+            url = url.strip().rstrip("/")
+            if not (url.startswith("http://") or url.startswith("https://")):
+                url = "https://" + url
+
+        t = options.get("logger", None) if options is not None else None
+        if t is None and not isinstance(t, Logger):
+            import logging
+            self.logger: Logger = logging.getLogger("console")
+        else:
+            self.logger: Logger = typing.cast(Logger, t)
+
+        self.__url = url + "/rest/"
+        self.__oauth_url = url + "/o/token/"
+
+        self.logger.debug(f"URL: {self.__url}")
+
+        self.client = httpx.AsyncClient()
+
+        CLIENT_ID = "ValispaceREST"
+
+        result = self.client.post(self.__oauth_url, data={
+            "grant_type": "password",
+            "username": username,
+            "password": password,
+            "client_id": CLIENT_ID,
+        })
+
+
+class Project:
+    def __init__(self, valispace: Valispace):
+        if not valispace:
+            raise ValispaceException("valispace instance can't be none")
+
+
+class Component:
+    def __init__(self, valispace: Valispace):
+        if not valispace:
+            raise ValispaceException("valispace instance can't be none")
+
+
+class Vali:
+    def __init__(self, valispace: Valispace):
+        if not valispace:
+            raise ValispaceException("valispace instance can't be none")
+
+
+class Matrix:
+    def __init__(self, valispace: Valispace):
+        if not valispace:
+            raise ValispaceException("valispace instance can't be none")
+
+
+class TextVali:
+    def __init__(self, valispace: Valispace):
+        if not valispace:
+            raise ValispaceException("valispace instance can't be none")
+
+
+class Specification:
+    def __init__(self, valispace: Valispace):
+        if not valispace:
+            raise ValispaceException("valispace instance can't be none")
+
+
+class Requirement:
+    def __init__(self, valispace: Valispace):
+        if not valispace:
+            raise ValispaceException("valispace instance can't be none")
+
+
+# vvv OLD STUFF vvv
+
+
 import getpass
 import json
 import requests
