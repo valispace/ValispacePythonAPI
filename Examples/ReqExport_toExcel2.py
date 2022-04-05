17e339d3fe629e7389478b8f3f88d022cbfe8395
Cauê Napier <cauenapier@users.noreply.github.com>
2022-04-05 13:04:30 +0100

    exporting component vm attachments

--- Examples/ReqExport_toExcel2.py
+++ Examples/ReqExport_toExcel2.py
@@ -6,16 +6,18 @@ import xlsxwriter
 # START OF CONFIG -------------------------------------
 deployment_name = ""
 
-fileName = "" 
+fileName = ""
 
 # Project ID
-project_ID = 
+project_ID =
 # Specification ID
 # If you want to export all Specifications on that project, use 0 as the ID
-specification_ID = 
+specification_ID =
 
 # Target File - Add or remove any field you want to get exported.
-fields = ['identifier', "title", "text", "specification", "rationale", "state", "type", "parents", "children", 'verification method', 'components','closeout reference', 'status','verification comment', 'verified on', 'verified by']
+# For Requirements attachments use 'attachments'
+# For Verification Components attachments use 'cvm_attachments'
+fields = ['identifier', "title", "text", "specification", "rationale", "state", "type", "parents", "children", 'verification method', 'components','closeout reference', 'status','verification comment', 'verified on', 'verified by', 'attachments', 'cvm_attachments']
 
 
 standardSeparator = ', '
@@ -53,21 +55,21 @@ if 'state' in fields:
 	idMappingName['state'] = stateList
 if 'tags' in fields:
 	# Get States
-	tagsList = valispace.get('tags/?project='+str(project_ID))
-	idMappingName['tags'] = tagsList
-if 'type' in fields:	
+	tagList = valispace.get('tags/?project='+str(project_ID))
+	idMappingName['tags'] = tagList
+if 'type' in fields:
 	# Get Types
 	typeList = valispace.get('requirements/types/?project='+str(project_ID))
 	idMappingName['type'] = typeList
-if 'specification' in fields:	
+if 'specification' in fields:
 	# Get Types
 	specificationList = valispace.get('requirements/specifications/?project='+str(project_ID))
 	idMappingName['specification'] = specificationList
-if 'section' in fields:	
+if 'section' in fields:
 	# Get Types
 	sectionList = valispace.get('requirements/groups/?project='+str(project_ID))
 	idMappingName['group'] = sectionList
-if 'compliance' or 'custom compliance' in fields:	
+if 'compliance' or 'custom compliance' in fields:
 	# Get Types
 	complianceList = valispace.get('requirements/compliance-statements/?project='+str(project_ID))
 	idMappingName['compliance'] = complianceList
@@ -80,30 +82,30 @@ if 'closeout reference' in fields:
 	analysisList = valispace.get('analyses/?project='+str(project_ID))
 	testList = valispace.get('testing/test-procedures/?project='+str(project_ID))
 	testStepsList = valispace.get('testing/test-procedure-steps/?project='+str(project_ID))
+if 'cvm_attachments' in fields:
+	# Get Complete List of Files
+	cvm_attachmentList = valispace.get('attachments')
 if 'owner' in fields or 'verified by' in fields:
 	userList = valispace.get('user')
 	idMappingName['users'] = userList
-# Generate Req Mapping - id to identifier
-# reqMapping = {}
-# for req in requirementList:
-# 	reqMapping[req["id"]] = req["identifier"]
 
 # Generate Req Mapping - id to identifier
-# filesMapping = {}
-# for file in filesList:
-# 	# Object Id of a File represents the parent to which the file is attached to
-# 	if file["file_type"] == 1 or file["file_type"] == 2:
-# 		if file["object_id"] not in filesMapping:
-# 			filesMapping[file["object_id"]] = [file["download_url"]]
-# 		else:
-# 			filesMapping[file["object_id"]].append(file["download_url"])
-# 	# Reference Link to another file
-# 	if file["file_type"] == 3:
-# 		download_url = [obj for obj in filesList if obj['id']==file["reference_file"]][0]['download_url']
-# 		if file["object_id"] not in filesMapping:
-# 			filesMapping[file["object_id"]] = [download_url]
-# 		else:
-# 			filesMapping[file["object_id"]].append(download_url)
+filesMapping = {}
+req_to_files = {}
+for file in fileList:
+	# Object Id of a File represents the parent to which the file is attached to
+	if file["file_type"] == 1 or file["file_type"] == 2:
+		if file["object_id"] not in filesMapping:
+			filesMapping[file["object_id"]] = [file["name"]]
+		else:
+			filesMapping[file["object_id"]].append(file["name"])
+	# Reference Link to another file
+	if file["file_type"] == 3:
+		file_name = [obj for obj in fileList if obj['id']==file["reference_file"]][0]['name']
+		if file["object_id"] not in filesMapping:
+			filesMapping[file["object_id"]] = [file_name]
+		else:
+			filesMapping[file["object_id"]].append(file_name)
 
 col = 0
 for field in fields:
@@ -136,14 +138,14 @@ def main():
 	row_num = 1
 	for req in requirementList:
 		row = dict.fromkeys(fields, "")
-		
+
 		if req["specification"] == specification_ID or specification_ID==0 :
 
 			for field in fields:
-				if field in directMappingFields: 
+				if field in directMappingFields:
 					row[field] = req[field]
-					
-				elif field in idMappingFields and req[field] != None: 
+
+				elif field in idMappingFields and req[field] != None:
 					row[field] = next((object_['name'] for object_ in idMappingName[field] if object_['id'] == req[field]), "")
 				elif field in idMultipleMappingFields:
 					output = ""
@@ -151,7 +153,7 @@ def main():
 					for item in itemList:
 						if output != "" : output += standardSeparator
 						if field in ['parents', 'children']:
-							output += next((object_['identifier'] for object_ in requirementList if object_['id'] == item), "") 
+							output += next((object_['identifier'] for object_ in requirementList if object_['id'] == item), "")
 					row[field] = output
 
 				elif field in directMultipleMappingFields:
@@ -161,7 +163,7 @@ def main():
 						if output != "" : output += standardSeparator
 						output += item
 					row[field] = output
-					
+
 				elif field == 'owner' and req['owner'] != None:
 					user= next((object_ for object_ in idMappingName['users'] if object_['id'] == req['owner']['id']), "")
 					if user != '':
@@ -169,7 +171,7 @@ def main():
 							row['owner'] += user['username']
 						else:
 							row['owner'] += user['first_name'] + ' ' + user['last_name']
-					
+
 				elif field == '#verified methods':
 					row['#verified methods'] = "{:s}/{:s}".format(str(req['verified_items']),str(req['total_items']))
 				elif field == '#verified children':
@@ -183,7 +185,7 @@ def main():
 						else:
 							if row["verification method"] != "" : row["verification method"] += verificationMethodSeparator
 							row["verification method"] +=  verification["method"]["name"]
-						
+
 						if 'components' in fields:
 							if row["components"] != "": row["components"] += verificationMethodSeparator
 							if 'closeout reference' in fields and row["closeout reference"] != "" : row["closeout reference"] += verificationMethodSeparator
@@ -191,29 +193,38 @@ def main():
 
 							for componentVM in verification["component_vms"]:
 								if row["components"] != "" and not row["components"].endswith('\n'): row["components"] += componentsSeparator
-								row["components"] +=  next((object_['name'] for object_ in componentsList if object_['id'] == componentVM["component"]), "") 
+								row["components"] +=  next((object_['name'] for object_ in componentsList if object_['id'] == componentVM["component"]), "")
 
 								if 'closeout reference' in fields:
 									if verification["method"] != None and verification["method"]["name"] == "Rules":
 										if row["closeout reference"] != "" and not row["closeout reference"].endswith('\n'): row["closeout reference"] += componentsSeparator
-										row["closeout reference"] += "Rules"	
-									else:							
+										row["closeout reference"] += "Rules"
+									else:
 										contentType = componentVM['content_type']
 										if row["closeout reference"] != "" and not row["closeout reference"].endswith('\n'): row["closeout reference"] += componentsSeparator
-										
+
 										if componentVM['content_type'] == 171: # File
-											row["closeout reference"] += next((object_['name'] for object_ in fileList if object_['id'] == componentVM['object_id']), "") 
+											row["closeout reference"] += next((object_['name'] for object_ in fileList if object_['id'] == componentVM['object_id']), "")
 										elif componentVM['content_type'] == 248: # Test
-											testStep = next((testStep for testStep in testStepsList if testStep['id'] == componentVM['object_id']), "") 
-											testProcedure = next((testProcedure for testProcedure in testList if testProcedure['id'] == testStep['test_procedure']), "") 
+											testStep = next((testStep for testStep in testStepsList if testStep['id'] == componentVM['object_id']), "")
+											testProcedure = next((testProcedure for testProcedure in testList if testProcedure['id'] == testStep['test_procedure']), "")
 											row["closeout reference"] += "{:s}: {:s}".format(testStep['title'], testProcedure['name'])
 										elif componentVM['content_type'] == 82: # Analysis
-											row["closeout reference"] += next((object_['name'] for object_ in analysisList if object_['id'] == componentVM['object_id']), "") 
+											row["closeout reference"] += next((object_['name'] for object_ in analysisList if object_['id'] == componentVM['object_id']), "")
 
-										
-										# row["closeout reference"] = componentVM['object_id']
+								if 'cvm_attachments' in fields:
+									if componentVM["vattachments"]:
+										if row["cvm_attachments"] != "" and not row["closeout reference"].endswith('\n'): row["closeout reference"] += componentsSeparator
+										cvm_attachment = next((attachment for attachment in cvm_attachmentList if attachment['id'] == componentVM["vattachments"][0]),None)
+										if cvm_attachment['target_type'] == 171: # File
+											row["cvm_attachments"] += next((object_['name'] for object_ in fileList if object_['id'] == cvm_attachment['object_id']), "")
+										if cvm_attachment['target_type'] == 82: # Analysis
+											row["cvm_attachments"] += next((object_['name'] for object_ in analysisList if object_['id'] == cvm_attachment['object_id']), "")
+									else:
+										if row["cvm_attachments"] != "" and not row["closeout reference"].endswith('\n'): row["closeout reference"] += componentsSeparator
+
 								if 'verified by' in fields:
-									user = next((object_ for object_ in userList if object_['id'] == componentVM["verified_by"]), "") 
+									user = next((object_ for object_ in userList if object_['id'] == componentVM["verified_by"]), "")
 									if row["verified by"] != "" and not row["verified by"].endswith('\n'): row["verified by"] += componentsSeparator
 									if user != '':
 										if user['first_name'] == '':
@@ -224,10 +235,10 @@ def main():
 
 							if 'status' in fields:
 								row["status"] = VMComponentPropertiesDirect(componentVM, row['status'], 'status', "-")
-						
+
 							if 'verification comment' in fields:
 								row["verification comment"] = VMComponentPropertiesDirect(componentVM, row['verification comment'], 'comment', "")
-							
+
 							if 'custom compliance' in fields:
 								row["custom compliance"] = VMComponentPropertiesID(componentVM, row['custom compliance'], 'custom_compliance_statement', complianceList, "")
 							if 'custom compliance comment' in fields:
@@ -235,30 +246,19 @@ def main():
 							if 'verification tags' in fields:
 								if row["verification tags"] != "": row["verification tags"] += verificationMethodSeparator
 								output = ""
-								tagsList = componentVM['tags']
-								for item in tagsList:
+								tagList = componentVM['tags']
+								for item in tagList:
 									if output != "" : output += standardSeparator
 									output += item
 								row["verification tags"] += output
 
+				elif field == 'attachments':
+					row['attachments'] = ', '.join(map(str, filesMapping[req['id']]))
 
-				
+
 			for field in fields:
 				worksheet.write(row_num, fields.index(field), row[field], cell_format)
 
-			# # Add File URL if there is any file attached to this requirement
-			# if req["id"] in filesMapping:
-			# 	for file in filesMapping[req["id"]]:
-			# 		if row["attachments"] != "" : row["attachments"] += "\n"
-			# 		row["attachments"] += file
-
-
-
-			# col = 0
-			# for field in fields:	
-			# worksheet.write(row_num, col, row[field], cell_format)
-			# 	col += 1
-			
 			row_num += 1
 
 	workbook.close()
@@ -274,7 +274,7 @@ def VMComponentPropertiesDirect(componentVM, row, field, empty):
 def VMComponentPropertiesID(componentVM, row, field, list, empty):
 	if row != "": row += verificationMethodSeparator
 	if componentVM[field] != None:
-		object_ = next((object_ for object_ in list if object_['id'] == componentVM[field]), "") 
+		object_ = next((object_ for object_ in list if object_['id'] == componentVM[field]), "")
 		row += object_['name']
 	else:
 		row += empty
