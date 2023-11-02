59709a96c32543428055fd07b10ef96e8082972d
Gonçalo Dias <goncalo.dias@valispace.com>
2023-11-02 14:10:45 +0000

    VALI-6743 release new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='valispace',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['requests', 'six'],
-    version='0.1.17',
+    version='0.1.18',
     description='Valispace Python API',
     author='Valispace',
     author_email='support@valispace.com',
