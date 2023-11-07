088a9cf343a77db8476bb60566e6a9c3a49637a9
Gonçalo Dias <goncalo.dias@valispace.com>
2023-11-07 14:58:03 +0000

    bump version 0.1.20

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='valispace',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['requests', 'six'],
-    version='0.1.19',
+    version='0.1.20',
     description='Valispace Python API',
     author='Valispace',
     author_email='support@valispace.com',
